a801b723d0195b1102d73ead8017f735489314e0
241B067 <241B067@juet.co.in>
2026-08-03 12:46:00 +0530

    Added two java files

---
+++ Lab1/Main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	  Add a1 = new Add();
+	  int res = a1.sum(5,6);
+	  System.out.println("Addition:" + res);  
+	}	
+}
---
+++ Lab1/add.java
@@ -0,0 +1,5 @@
+class Add{
+    public int sum(int n1, int n2){
+        return n1+n2;
+    }
+}
